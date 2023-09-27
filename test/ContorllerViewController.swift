c4984529f4ba4ea0a5f9846bd94dac4a5d21daeb
Denim <dagn456@mail.ru>
2023-09-27 11:13:46 +0300

    TestCommit

---
+++ test/ContorllerViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ContorllerViewController.swift
+//  test
+//
+//  Created by Деним Мержан on 22.09.23.
+//
+
+import UIKit
+
+class ContorllerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        indicator.startAnimating()
+        getImage()
+        
+    }
+    
+    func getImage(){
+        guard let url = URL(string: "https://static.fotogora.ru/fotogora/wysiwyg/wp-content/uploads/2015/06/by-Plastikete.jpg") else {return}
+        let queue = DispatchQueue.global(qos: .utility)
+        queue.async {
+            if let data = try? Data(contentsOf: url) {
+                DispatchQueue.main.async {
+                    self.imageView.image = UIImage(data: data)
+                }
+                
+            }
+        }
+    }
+}
+
+
---
+++ test/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCellCell.swift
+//  test
+//
+//  Created by Деним Мержан on 22.09.23.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
